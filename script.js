894e9c5c0d7566d136d17623d5c324b1d7a0f229
Ludovic <luluorban2@hotmail.com>
2022-10-28 17:42:57 +0200

    Finished project
    Project done ( as per description )

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log("Hello");
+function getComputerChoice(){
+    min = 1;
+    max = 3;
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (result == 1){
+        return "rock"
+    }
+    else if (result == 2){
+        return "paper"
+    }
+    else{
+        return "scissors"
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    switch(true){
+
+        case playerSelection == computerSelection:
+            return "No one looses"
+
+        case playerSelection == "rock" && computerSelection == "scissors" ||
+            playerSelection == "scissors" && computerSelection == "paper"||
+            playerSelection == "paper" && computerSelection == "rock":
+            result++
+            return `Congrat ${playerSelection} beats ${computerSelection}`
+
+        case playerSelection == "rock" && computerSelection == "paper" ||
+            playerSelection == "scissors" && computerSelection == "rock" ||
+            playerSelection == "paper" && computerSelection == "scissors":
+            return `Too bad ${computerSelection} beats ${playerSelection}`
+
+        default:
+            return `bug - ${playerSelection} ${computerSelection}`
+    }
+}
+
+function game(){
+    console.log(playRound(playerSelection,computerSelection))
+}
+
+
+
+let playerSelection
+let computerSelection
+let result = 0
+
+for (let i = 0; i < 5; i++) {
+    computerSelection = getComputerChoice().toString()
+    playerSelection = prompt("Rock, paper or scissors ?").toLowerCase()
+    game()
+    console.log(`Current score: ${result}`)
+}
